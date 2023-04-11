caf5bd8a106976bce4de385cae0b0500e59fd72b
Renan Siqueira <renan.siqueira8a@gmail.com>
2023-04-11 01:07:09 -0300

    empty list articles fix

--- client/src/components/Main/main.jsx
+++ client/src/components/Main/main.jsx
@@ -54,29 +54,6 @@ function App() {
             :
               <h1>Nenhum artigo</h1>
             }
-            {/* {articles.map((article, index) => (
-              <div key={index} className={`${styles.thumbarticle_container} d-flex flex-column`}>
-                <Link className={styles.autortitle} to={`/article/${article.slug}`}>
-                  <h3 className="mb-0">{article.title}</h3>
-                </Link>
-                <div className={`mb-1 ${styles.autordetails}`}>
-                  Autor: {article.author}, Criado em{" "}
-                  {new Date(article.createdAt).toLocaleDateString()}
-                </div>
-                <p className={`card-text mb-auto ${styles.article_content}`}>{domPurify.sanitize(article.content, { USE_PROFILES: { html: false } })}</p>
-                
-                <div className="d-flex justify-content-between align-items-center">
-                  <Link className={`${styles.readarticle} align-self-center`} to={`/article/${article.slug}`}>
-                    <p className="">Continuar Lendo</p>
-                  </Link>
-                  
-                </div>
-                <div className="btn-group btn-group-sm">
-                  <button type="button" className="btn btn-sm btn-outline-success">Editar</button>
-                  <button type="button" className="btn btn-sm btn-outline-danger">Excluir</button>
-                </div>
-              </div>
-            ))} */}
           </div>
         </div>
     </div>
